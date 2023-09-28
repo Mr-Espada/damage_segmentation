b609d32f889d3dddffc959a2e8b38c4f9e466ad9
Night^^Stalker <67825314+mehdinejjar86@users.noreply.github.com>
2023-09-28 19:53:54 +0100

    Create overlay_classses.py

---
+++ overlay_classses.py
@@ -0,0 +1,36 @@
+def overlay_transparent(bg_img, img_to_overlay_t):
+    # Extract the alpha mask of the RGBA image, convert to RGB 
+    b,g,r,a = cv2.split(img_to_overlay_t)
+    overlay_color = cv2.merge((b,g,r))
+
+    mask = cv2.medianBlur(a,5)
+
+    # Black-out the area behind the logo in our original ROI
+    img1_bg = cv2.bitwise_and(bg_img.copy(),bg_img.copy(),mask = cv2.bitwise_not(mask))
+
+    # Mask out the logo from the logo image.
+    img2_fg = cv2.bitwise_and(overlay_color,overlay_color,mask = mask)
+
+    # Update the original image with our new ROI
+    bg_img = cv2.add(img1_bg, img2_fg)
+
+    return bg_img
+
+def black_to_transparent(img):
+        # Convert image to image gray
+        tmp = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Applying thresholding technique
+        _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+
+        # Using cv2.split() to split channels 
+        # of coloured image
+        b, g, r = cv2.split(img)
+
+        # Making list of Red, Green, Blue
+        # Channels and alpha
+        rgba = [b, g, r, alpha]
+
+        # Using cv2.merge() to merge rgba
+        # into a coloured/multi-channeled image
+        return cv2.merge(rgba, 4)
